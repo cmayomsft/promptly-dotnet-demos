b745a24059670ef29c9aa3a18d86f287bc6f8e03
Chris Mayo <cmayo@microsoft.com>
2018-03-25 08:33:27 +0900

    Added default page to Topics demo.

--- Topics/Topics/RootTopic.cs
+++ Topics/Topics/RootTopic.cs
@@ -12,7 +12,6 @@ namespace Topics.Topics
     public class RootTopic : TopicsRoot<BotConversationState>
     {
         private const string ADD_ALARM_TOPIC = "addAlarmTopic";
-        private const string DELETE_ALARM_TOPIC = "deleteAlarmTopic";
 
         public RootTopic(IBotContext context) : base(context)
         {
@@ -47,39 +46,6 @@ namespace Topics.Topics
 
                 return addAlarmTopic;
             });
-
-            this.SubTopics.Add(DELETE_ALARM_TOPIC, (object[] args) =>
-            {
-                var alarms = (args.Length > 0) ? (List<Alarm>)args[0] : null;
-
-                var deleteAlarmTopic = new DeleteAlarmTopic(alarms);
-
-                deleteAlarmTopic.Set
-                    .OnSuccess((ctx, value) =>
-                        {
-                            this.ClearActiveTopic();
-
-                            if (!value.DeleteConfirmed)
-                            {
-                                context.SendActivity($"Ok, I won't delete alarm '{ value.Alarm.Title }'.");
-                                return;
-                            }
-
-                            ctx.GetUserState<BotUserState>().Alarms.RemoveAt(value.AlarmIndex);
-
-                            context.SendActivity($"Done. I've deleted alarm '{ value.Alarm.Title }'.");
-                        })
-                    .OnFailure((ctx, reason) =>
-                        {
-                            this.ClearActiveTopic();
-
-                            context.SendActivity("Let's try something else.");
-
-                            this.ShowDefaultMessage(context);
-                        });
-
-                return deleteAlarmTopic;
-            });
         }
 
         public override Task OnReceiveActivity(IBotContext context)
@@ -94,29 +60,6 @@ namespace Topics.Topics
                     // Set the active topic and let the active topic handle this turn.
                     this.SetActiveTopic(ADD_ALARM_TOPIC)
                             .OnReceiveActivity(context);
-                    return Task.CompletedTask;
-                }
-
-                if (message.Text.ToLowerInvariant() == "delete alarm")
-                {
-                    this.SetActiveTopic(DELETE_ALARM_TOPIC, context.GetUserState<BotUserState>().Alarms)
-                        .OnReceiveActivity(context);
-                    return Task.CompletedTask;
-                }
-
-                if (message.Text.ToLowerInvariant() == "show alarms")
-                {
-                    this.ClearActiveTopic();
-
-                    AlarmsView.ShowAlarms(context, context.GetUserState<BotUserState>().Alarms);
-                    return Task.CompletedTask;
-                }
-
-                if (message.Text.ToLowerInvariant() == "help")
-                {
-                    this.ClearActiveTopic();
-
-                    this.ShowHelp(context);
                     return Task.CompletedTask;
                 }
 
@@ -135,18 +78,7 @@ namespace Topics.Topics
 
         private void ShowDefaultMessage(IBotContext context)
         {
-            context.SendActivity("'Show Alarms', 'Add Alarm', 'Delete Alarm', 'Help'.");
-        }
-
-        private void ShowHelp(IBotContext context)
-        {
-            var message = "Here's what I can do:\n\n";
-            message += "To see your alarms, say 'Show Alarms'.\n\n";
-            message += "To add an alarm, say 'Add Alarm'.\n\n";
-            message += "To delete an alarm, say 'Delete Alarm'.\n\n";
-            message += "To see this again, say 'Help'.";
-
-            context.SendActivity(message);
+            context.SendActivity("'Add Alarm'.");
         }
     }
 }
